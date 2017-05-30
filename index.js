8007412ecc1631fd50c85858e62f35cb4086dac7
joshwilcken <joshuawilcken@Joshuas-MacBook-Pro.local>
2017-05-30 15:20:02 -0600

    uploading finished product to github

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const messages = [];
+
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+
+
+
+app.get('/messages', function (req, res, next) {
+    res.status(200).json({ messages: messages });
+  });
+
+ app.post('/messages', function (req, res, next) {
+    messages.push({ message: req.body.message, time: new Date() });
+    res.status(200).json({ messages: messages });
+  });
+
+  
+
+
+app.listen(3000);
+
 
